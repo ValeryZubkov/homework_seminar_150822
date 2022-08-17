6f7f2ae41b66d1d1209416e00bbb8c878613044c
Valery Zubkov <bouddha@mail.ru>
2022-08-18 00:00:40 +0400

    homework number 3

---
+++ Homework_003/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу копирования массива
+//Console.WriteLine("Введите размер массива: ");
+//int n = int.Parse(Console.ReadLine() ?? "0");
+
+void CopyArray(int[] arr, int[] arr1, int size)
+{   
+    for(int i = 0; i < size; i++)
+    {
+      arr1[i] = arr[i]; 
+      Console.Write($"{arr1[i]} ");
+    }
+}
+int[] array = {45, 55, 3, 56, 7, 9, 87, 12, 2};
+int length = array.Length;
+int[] newarray = new int[length];
+Console.Write("Новый скопированный массив newarray состоит из следующих элементов: ");
+CopyArray(array, newarray, length);
+Console.WriteLine();
